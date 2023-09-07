1c52b68fe152414a806716ba0c7682eeada391c6
mbali92 <83025186+mbali92@users.noreply.github.com>
2023-09-07 11:58:34 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-
 import './App.css';
-
+import Dashboard from './components/Dashboard';
 function App() {
+
+   //global states are declared here please
+  //  functions
+  
    //global states
+
   return (
-     //routing 
-    <>app</>
+     //routing
+
+     <div className='App'>
+        <Dashboard/>
+        <h1>hi</h1>
+     </div>
+
    
   );
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,1 +1,10 @@
 //profile info 
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
